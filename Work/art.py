78ba819e19d4f30568425fcac5fd181062bb69ae
lusm <loveyousomuch554@gmail.com>
2023-08-09 20:55:29 +0300

    space

--- Work/art.py
+++ Work/art.py
@@ -9,6 +9,7 @@ def draw(rows, columns):
   for r in range(rows):
     print(''.join(random.choice(chars) for _ in range(columns)))
 
+
 if __name__ == '__main__':
   if len(sys.argv) != 3:
     raise SystemExit("Usage: art.py rows columns")
