fa0d7c7b3fc48d7e2ad2f541f17c5f0546c52d41
lusm <loveyousomuch554@gmail.com>
2023-08-10 18:15:48 +0300

    Exercise 2.1 - (d) Memory Use of Other Data Structures

--- Work/readrides.py
+++ Work/readrides.py
@@ -2,16 +2,18 @@
 
 import csv
 
-def as_tuples(route, date, daytype, rides):
-  return (route, date, daytype, rides)
+def read_rides_as_tuples(filename):
+	record_structure = lambda route, date, daytype, rides: (route, date, daytype, rides)
+	return read_rides(record_structure, filename)	
 
-def as_dict(route, date, daytype, rides):
-	return {
+def read_rides_as_dicts(filename):
+	record_structure = lambda route, date, daytype, rides: {
     'route': route,
     'date': date,
     'daytype': daytype,
-    'rides': rides,
-	}
+    'rides': rides
+  }
+	return read_rides(record_structure, filename)	
 
 class Row:
   def __init__(self, route, date, daytype, rides):
@@ -20,18 +22,18 @@ class Row:
     self.daytype = daytype
     self.rides = rides
 
-def as_class(route, date, daytype, rides):
-  return Row(route, date, daytype, rides)
-
+def read_rides_as_classes(filename):
+	record_structure = lambda route, date, daytype, rides: Row(route, date, daytype, rides)
+	return read_rides(record_structure, filename)	
 
 from collections import namedtuple
-Row = namedtuple('Row', ['route', 'date', 'daytype', 'rides']) 
+RowNamedT = namedtuple('Row', ['route', 'date', 'daytype', 'rides']) 
 
-def as_namedtuple(route, date, daytype, rides):
-  return Row(route, date, daytype, rides) 
+def read_rides_as_namedtuples(filename):
+	record_structure = lambda route, date, daytype, rides: RowNamedT(route, date, daytype, rides)
+	return read_rides(record_structure, filename)	
 
-
-class RowS:
+class RowSlot:
   __slots__ = ['route', 'date', 'daytype', 'rides']
   def __init__(self, route, date, daytype, rides):
     self.route = route
@@ -39,8 +41,9 @@ class RowS:
     self.daytype = daytype
     self.rides = rides
 
-def as_class_slots(route, date, daytype, rides):
-  return RowS(route, date, daytype, rides) 
+def read_rides_as_namedtuples(filename):
+	record_structure = lambda route, date, daytype, rides: RowSlot(route, date, daytype, rides)
+	return read_rides(record_structure, filename)	
 
 def read_rides(data_structure, filename):
   records = []
@@ -56,16 +59,15 @@ def read_rides(data_structure, filename):
 if __name__ == '__main__':
   import tracemalloc
   
-  def rides_traced_memory(method):
+  def rides_traced_memory(read_function):
     tracemalloc.start()
-    rows = read_rides(method, 'Data/ctabus.csv')
+    rows = read_function('Data/ctabus.csv')
     current, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
     return current, peak
-    
 
-  print('Memory Use As Tuples: Current {:,}, Peak {:,}'.format(*rides_traced_memory(as_tuples)))
-  print('Memory Use As Dict: Current {:,}, Peak {:,}'.format(*rides_traced_memory(as_dict)))
-  print('Memory Use As Class Instance: Current {:,}, Peak {:,}'.format(*rides_traced_memory(as_class)))
-  print('Memory Use As Named Tuple: Current {:,}, Peak {:,}'.format(*rides_traced_memory(as_namedtuple)))
-  print('Memory Use As Class Slots: Current {:,}, Peak {:,}'.format(*rides_traced_memory(as_class_slots)))
+  print('Memory Use As Tuples: Current {:,}, Peak {:,}'.format(*rides_traced_memory(read_rides_as_tuples)))
+  print('Memory Use As Dicts: Current {:,}, Peak {:,}'.format(*rides_traced_memory(read_rides_as_dicts)))
+  print('Memory Use As Class Instance: Current {:,}, Peak {:,}'.format(*rides_traced_memory(read_rides_as_classes)))
+  print('Memory Use As Named Tuple: Current {:,}, Peak {:,}'.format(*rides_traced_memory(read_rides_as_namedtuples)))
+  print('Memory Use As Class Slots: Current {:,}, Peak {:,}'.format(*rides_traced_memory(read_rides_as_namedtuples)))
