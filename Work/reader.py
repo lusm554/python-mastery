acad723fb6faefc514c5e304ec2370286f2e2f7d
lusm <loveyousomuch554@gmail.com>
2023-08-14 14:33:16 +0300

    Exercise 2.6 - (b) Making a Parsing Utility Function

--- Work/reader.py
+++ Work/reader.py
@@ -1,1 +1,18 @@
 # reader.py
+
+import csv
+
+def read_csv_as_dicts(filename, types):
+  records = []
+  with open(filename, 'r') as f:
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
+      records.append({ key: cast(val) for key, val, cast in zip(headers, row, types) })
+
+  return records
+
+if __name__ == '__main__':
+  port = read_csv_as_dicts('Data/portfolio.csv', [str, int, float])
+  for s in port:
+    print(s)
