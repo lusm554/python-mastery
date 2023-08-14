20264462a0193d6181c68817743b7c7220a1a2e3
lusm <loveyousomuch554@gmail.com>
2023-08-14 17:46:22 +0300

    Exercise 3.1 - (b) Reading a portfolio

--- Work/stock.py
+++ Work/stock.py
@@ -1,5 +1,7 @@
 # stock.py
 
+import csv
+
 class Stock:
   def __init__(self, name, shares, price):
     self.name = name
@@ -11,3 +13,19 @@ class Stock:
   
   def sell(self, sellshares):
     self.shares -= sellshares
+  
+
+def read_portfolio(filename):
+  records = []
+  with open(filename, 'r') as f:
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
+      record = Stock(row[0], int(row[1]), float(row[2]))
+      records.append(record) 
+  return records
+
+if __name__ == "__main__":
+  from pprint import pprint
+  portfolio = read_portfolio('Data/portfolio.csv')
+  pprint(portfolio[:10])
