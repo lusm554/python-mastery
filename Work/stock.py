15f6de17fd21d4705c478ef32f82e1f6ce58cf2d
lusm <loveyousomuch554@gmail.com>
2023-08-15 18:56:33 +0300

    Exercise 3.4 - (c) Enforcing Validation Rules

--- Work/stock.py
+++ Work/stock.py
@@ -3,6 +3,7 @@
 import csv
 
 class Stock:
+  __slots__ = ('name', '_shares', '_price')
   _types = (str, int, float)
   def __init__(self, name, shares, price):
     self.name = name
@@ -17,10 +18,34 @@ class Stock:
   @property
   def cost(self):
     return self.shares * self.price
-  
+
+  @property
+  def shares(self):
+    return self._shares 
+
+  @shares.setter
+  def shares(self, value):
+    if not isinstance(value, self._types[1]):
+      raise TypeError(f'Expected {self._types[1].__name__}')
+    if not value >= 0:
+      raise TypeError('Expected shares >= 0')
+    self._shares = value
+
+  @property
+  def price(self):
+    return self._price
+
+  @price.setter
+  def price(self, value):
+    if not isinstance(value, self._types[2]):
+      raise TypeError(f'Expected {self._types[2].__name__}')
+    if not value >= 0:
+      raise TypeError('Expected price >= 0')
+    self._price = value
+     
   def sell(self, sellshares):
     self.shares -= sellshares
-  
+
 
 def read_portfolio(filename, cls):
   records = []
