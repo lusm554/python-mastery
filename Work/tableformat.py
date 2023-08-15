aa32b663d76d39ded20c43ed70e6eeaa3fe46f22
lusm <loveyousomuch554@gmail.com>
2023-08-15 19:46:05 +0300

    Exercise 3.5 - (b) Implementing a concrete formatter

--- Work/tableformat.py
+++ Work/tableformat.py
@@ -7,9 +7,17 @@ class TableFormatter:
   def row(self, rowdata):
     raise NotImplementedError()
 
+class TextTableFormatter(TableFormatter):
+  def headings(self, headers):
+    print(' '.join('%10s' % h for h in headers))
+    print(('-'*10 + ' ')*len(headers))
+
+  def row(self, rowdata):
+    print(' '.join('%10s' % d for d in rowdata))
+
 def print_table(records, headers, formatter):
   formatter.headings(headers)
-  for s in data:
+  for r in records:
     rowdata = [getattr(r, fieldname) for fieldname in headers]
     formatter.row(rowdata)
 
