2eb3d60c735504b053c4fd75912bb2528f3535e4
lusm <loveyousomuch554@gmail.com>
2023-08-15 17:48:29 +0300

    Exercise 3.3 - (d) Generalizing

--- Work/reader.py
+++ Work/reader.py
@@ -11,7 +11,21 @@ def read_csv_as_dicts(filename, types):
       records.append({ key: cast(val) for key, val, cast in zip(headers, row, types) })
   return records
 
+def read_csv_as_instances(filename, cls):
+  '''
+  Read a CSV file into a list of instances
+  '''
+  records = []
+  with open(filename, 'r') as f:
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
+      record = cls.from_row(row)
+      records.append(record)
+  return records
+
 if __name__ == '__main__':
-  port = read_csv_as_dicts('Data/portfolio.csv', [str, int, float])
-  for s in port:
-    print(s)
+  from stock import Stock
+  from pprint import pprint
+  port = read_csv_as_instances('Data/portfolio.csv', Stock)
+  pprint(port[:5])
