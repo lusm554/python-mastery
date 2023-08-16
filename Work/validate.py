514a0a1b3bcc5ddc9a5376c2aaff762387bb31dd
lusm <loveyousomuch554@gmail.com>
2023-08-16 18:19:24 +0300

    Exercise 4.2 - (c) Using your validators

--- Work/validate.py
+++ Work/validate.py
@@ -44,3 +44,48 @@ class PositiveFloat(Float, Positive):
 
 class NonEmptyString(String, NonEmpty):
 	pass
+
+
+if __name__ == '__main__':
+  class Stock:
+    __slots__ = ('name', '_shares', '_price') # slots!
+    _types = (str, int, float)
+    def __init__(self, name, shares, price):
+      self.name = name
+      self.shares = shares
+      self.price = price
+
+    def __repr__(self):
+      return f'Stock(\'{self.name}\', {self.shares}, {self.price})'
+
+    def __eq__(self, other):
+      return isinstance(other, Stock) and ((self.name, self.shares, self.price) ==
+                                           (other.name, self.shares, other.price))
+
+    @classmethod
+    def from_row(cls, row):
+      ''' Alt constructor '''
+      values = [cast(val) for cast, val in zip(cls._types, row)]
+      return cls(*values)
+
+    @property
+    def cost(self):
+      return self.shares * self.price
+
+    @property
+    def shares(self):
+      return self._shares 
+    @shares.setter
+    def shares(self, value):
+      self._shares = PositiveInteger.check(value) 
+
+    @property
+    def price(self):
+      return self._price
+    @price.setter
+    def price(self, value):
+      self._price = PositiveFloat.check(value)
+       
+    def sell(self, sellshares):
+      self.shares -= sellshares
+
