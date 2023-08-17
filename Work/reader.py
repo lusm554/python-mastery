65d9d510e9890e8507815be500810b5fc3108135
lusm <loveyousomuch554@gmail.com>
2023-08-17 22:31:03 +0300

    Exercise 5.1 - (a) Back to Basics

--- Work/reader.py
+++ Work/reader.py
@@ -1,50 +1,38 @@
 # reader.py
 
 import csv
-from abc import ABC, abstractmethod
-
-class CSVParser(ABC):
-  def parse(self, filename):
-    records = []
-    with open(filename) as f:
-      rows = csv.reader(f)
-      headers = next(rows)
-      for row in rows:
-        record = self.make_record(headers, row)
-        records.append(record)
-    return records
-
-  @abstractmethod
-  def make_record(self, headers, row):
-    raise NotImplemented()
-
-class DictCSVParser(CSVParser):
-  def __init__(self, types):
-    self.types = types
-
-  def make_record(self, headers, row):
-    return { name: cast(val) for name, val, cast in zip(headers, row, self.types) }
-
-class InstanceCSVParser(CSVParser):
-  def __init__(self, cls):
-    self.cls = cls
-
-  def make_record(self, headers, row):
-    return self.cls.from_row(row)
 
 def read_csv_as_dicts(filename, types):
-  return DictCSVParser(types).parse(filename)
+  '''
+  Read CSV data into a list of dictionaries with optional type conversion
+  '''
+  records = []
+  with open(filename) as file:
+    rows = csv.reader(file)
+    headers = next(rows)
+    for row in rows:
+      record = { name: cast(val) for name, val, cast in zip(headers, row, types) }
+      records.append(record)
+  return records
 
 def read_csv_as_instances(filename, cls):
   '''
-  Read a CSV file into a list of instances
+  Read CSV data into a list of instances
   '''
-  return InstanceCSVParser(cls).parse(filename)
+  records = []
+  with open(filename) as file:
+    rows = csv.reader(file)
+    headers = next(rows)
+    for row in rows:
+      record = cls.from_row(row)
+      records.append(record)
+  return records
 
 if __name__ == '__main__':
   from stock import Stock
   from pprint import pprint
-  port = read_csv_as_instances('Data/portfolio.csv', Stock)
-  pprint(port[:5])
-  port = read_csv_as_dicts('Data/portfolio.csv', [str, int, float])
-  pprint(port[:5])
+  p = read_csv_as_dicts('Data/portfolio.csv', [str, int, float])
+  pprint(p) 
+
+  p = read_csv_as_instances('Data/portfolio.csv', Stock)
+  pprint(p)
