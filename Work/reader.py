ff4c3bd330af24d48ac907354b20a570fc5bd1e1
lusm <loveyousomuch554@gmail.com>
2023-08-17 23:07:02 +0300

    Exercise 5.1 - (b) Thinking about Flexibility

--- Work/reader.py
+++ Work/reader.py
@@ -2,31 +2,49 @@
 
 import csv
 
+
+def csv_as_dicts(lines, types):
+  '''
+  Convert data to list of dicts
+  '''
+  records = []
+  lines = csv.reader(lines)
+  headers = next(lines)
+  for line in lines:
+    print(list(zip(['name', 'shares', 'price'], line, types)))
+    record = { name: cast(val) for name, val, cast in zip(headers, line, types) }
+    records.append(record)
+  return records
+
+def csv_as_instances(lines, cls):
+  '''
+  Convert data to list of instances
+  '''
+  records = []
+  lines = csv.reader(lines)
+  headers = next(lines)
+  for line in lines:
+    record = cls.from_row(line)
+    records.append(record)
+  return records
+
 def read_csv_as_dicts(filename, types):
   '''
   Read CSV data into a list of dictionaries with optional type conversion
   '''
-  records = []
   with open(filename) as file:
-    rows = csv.reader(file)
-    headers = next(rows)
-    for row in rows:
-      record = { name: cast(val) for name, val, cast in zip(headers, row, types) }
-      records.append(record)
+    records = csv_as_dicts(file, [str, int, float])
   return records
+
 
 def read_csv_as_instances(filename, cls):
   '''
   Read CSV data into a list of instances
   '''
-  records = []
   with open(filename) as file:
-    rows = csv.reader(file)
-    headers = next(rows)
-    for row in rows:
-      record = cls.from_row(row)
-      records.append(record)
+    records = csv_as_instances(file, cls)
   return records
+
 
 if __name__ == '__main__':
   from stock import Stock
