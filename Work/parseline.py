cee719a65520bb9db38846d0d72040478e280e6f
lusm <loveyousomuch554@gmail.com>
2023-08-19 16:32:45 +0300

    Exercise 5.2 - (b) Returning Optional Values

--- Work/parseline.py
+++ Work/parseline.py
@@ -4,9 +4,16 @@ def parse_line(line: str) -> tuple:
   '''
   Parse line like name=value into tuple.
   '''
-  name, val = line.split('=')
+  try:
+    name, val = line.split('=')
+  except ValueError:
+    # Design discussion: Would it be better for the parse_line() function to raise an exception on malformed data?
+    # Yes. I think better raise exception when data incorrect. 
+    return None
   return name, val
 
 if __name__ == '__main__':
   res = parse_line('email=guido@python.org')
+  print(res)
+  res = parse_line('spam')
   print(res)
