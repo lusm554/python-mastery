5ca06ed9af67e5bfc3a4d8dbdbf9cb9b61fd3a40
lusm <loveyousomuch554@gmail.com>
2023-08-20 00:53:04 +0300

    Exercise 6.1 - (d) Starting Over

--- Work/stock.py
+++ Work/stock.py
@@ -5,6 +5,13 @@ from structure import Structure
 class Stock(Structure):
   _fields = ('name','shares','price')
 
+  @property
+  def cost(self):
+    return self.price * self.shares
+
+  def sell(self, nshares):
+    self.shares -= nshares
+  
 if __name__ == '__main__':
   s = Stock('GOOG',100,490.1)
   print(s)
