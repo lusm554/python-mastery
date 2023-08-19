9c345d283a208a546330733d728c7bcdc4378166
lusm <loveyousomuch554@gmail.com>
2023-08-20 00:50:41 +0300

    Exercise 6.1 - (c) Restricting Attribute Names

--- Work/structure.py
+++ Work/structure.py
@@ -7,6 +7,12 @@ class Structure:
       raise TypeError(f"Expected {len(self._fields)} arguments")
     for field, arg in zip(self._fields, args):
       setattr(self, field, arg)
+  
+  def __setattr__(self, name, val):
+    if name.startswith('_') or name in self._fields:
+      super().__setattr__(name, val)
+    else:
+      raise AttributeError("No attribute %s" % name)
 
   def __repr__(self):
     return "%s(%s)" % (self.__class__.__name__, ', '.join(repr(getattr(self, attr)) for attr in self._fields))
