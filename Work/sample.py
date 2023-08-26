9d69c24f8f659098b7dbba939f185564fcde0833
lusm <loveyousomuch554@gmail.com>
2023-08-26 21:11:52 +0300

    Exercise 7.2 - (b) Your first decorator with arguments

--- Work/sample.py
+++ Work/sample.py
@@ -1,6 +1,6 @@
 # sample.py
 
-from logcall import logged
+from logcall import logged, logformat
 
 @logged
 def add(x,y):
@@ -9,3 +9,12 @@ def add(x,y):
 @logged
 def sub(x,y):
   return x - y
+
+@logformat('{func.__code__.co_filename}:{func.__name__}')
+def mul(x, y):
+  return x * y
+
+if __name__ == '__main__':
+  add(1, 2)
+  sub(1, 1)
+  mul(2, 2)
