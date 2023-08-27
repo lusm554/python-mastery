7cfc30ce79dba91037a1e8d95260466c31640a85
lusm <loveyousomuch554@gmail.com>
2023-08-27 14:29:50 +0300

    unsolved: Exercise 7.2 - (c) Multiple decorators and methods

--- Work/sample.py
+++ Work/sample.py
@@ -2,6 +2,7 @@
 
 from logcall import logged, logformat
 
+"""
 @logged
 def add(x,y):
   return x + y
@@ -13,8 +14,35 @@ def sub(x,y):
 @logformat('{func.__code__.co_filename}:{func.__name__}')
 def mul(x, y):
   return x * y
+"""
 
 if __name__ == '__main__':
+  """
   add(1, 2)
   sub(1, 1)
   mul(2, 2)
+  """
+  class Spam:
+    @logged
+    def instance_method(self):
+        pass
+
+    @logged
+    @classmethod
+    def class_method(cls):
+        pass
+
+    @logged
+    @staticmethod
+    def static_method():
+        pass
+
+    @logged
+    @property
+    def property_method(self):
+        pass 
+  s = Spam()
+  s.instance_method()
+  Spam.class_method()
+  Spam.static_method()
+  print(s.property_method)
