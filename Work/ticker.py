4674da33ab62a65b6ad873569a34be2ae0392a51
lusm <loveyousomuch554@gmail.com>
2023-08-28 15:55:07 +0300

    Exercise 8.2 - (b) Making more pipeline components

---
+++ Work/ticker.py
@@ -0,0 +1,23 @@
+# ticker.py
+
+from structure import Structure
+
+class Ticker(Structure):
+  name = String()
+  price = Float()
+  date = String()
+  time = String()
+  change = Float()
+  open = Float()
+  high = Float()
+  low = Float()
+  volume = Integer()
+
+if __name__ == '__main__':
+  from follow import follow
+  import csv
+  lines = follow('Data/stocklog.csv')
+  rows = csv.reader(lines)
+  records = (Ticker.from_row(row) for row in rows)
+  for record in records:
+    print(record)
