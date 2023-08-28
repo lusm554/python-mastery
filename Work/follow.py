249196ac29635203e75f0f3927411a13c2745980
lusm <loveyousomuch554@gmail.com>
2023-08-28 22:18:14 +0300

    Exercise 8.4 - (a) Closing a Generator

--- Work/follow.py
+++ Work/follow.py
@@ -4,15 +4,17 @@ import os
 import time
 
 def follow(filename):
-  f = open(filename)
-  f.seek(0, os.SEEK_END) # move file pointer 0 byes from end of file
-
-  while True:
-    line = f.readline()
-    if line == '':
-      time.sleep(0.1) # sleep briefly and retry
-      continue
-    yield line
+  try: 
+    with open(filename, 'r') as f:
+      f.seek(0, os.SEEK_END) # move file pointer 0 byes from end of file
+      while True:
+        line = f.readline()
+        if line == '':
+          time.sleep(0.1) # sleep briefly and retry
+          continue
+        yield line
+  except GeneratorExit:
+    print('Following Done')
 
 if __name__ == '__main__':
   for line in follow('Data/stocklog.csv'):
@@ -20,4 +22,3 @@ if __name__ == '__main__':
     name, price, change = fields[0].strip('"'), float(fields[1]), float(fields[4])
     if change < 0:
       print('%10s %10.2f %10.2f' % (name, price, change))
-
