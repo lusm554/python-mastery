6f3f20aba18b46b11ab2603cca97cd1c5544f216
lusm <loveyousomuch554@gmail.com>
2023-08-30 16:04:10 +0300

    Exercise 8.6 - (c) Async/Await

--- Work/server.py
+++ Work/server.py
@@ -3,6 +3,7 @@
 from socket import *
 from select import select
 from collections import deque
+from types import coroutine
 
 tasks = deque()
 recv_wait = {}
@@ -35,15 +36,18 @@ class GenSocket:
   def __init__(self, sock):
     self.sock = sock
 
+  @coroutine
   def accept(self):
     yield 'recv', self.sock
     client, addr = self.sock.accept()
     return GenSocket(client), addr
 
+  @coroutine
   def recv(self, maxsize):
     yield 'recv', self.sock
     return self.sock.recv(maxsize)
 
+  @coroutine
   def send(self, data):
     yield 'send', self.sock
     return self.sock.send(data)
@@ -51,22 +55,22 @@ class GenSocket:
   def __getattr__(self, name):
     return getattr(self.sock, name)
 
-def tcp_server(address, handler):
+async def tcp_server(address, handler):
   sock = GenSocket(socket(AF_INET, SOCK_STREAM))
   sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
   sock.bind(address)
   sock.listen()
   while True:
-    client, addr = yield from sock.accept() 
+    client, addr = await sock.accept() 
     tasks.append(handler(client, addr))
 
-def echo_handler(client, address):
+async def echo_handler(client, address):
   print('Connection from', address)
   while True:
-    data = yield from client.recv(1000)
+    data = await client.recv(1000)
     if not data:
       break
-    yield from client.send(b'GOT:' + data)
+    await client.send(b'GOT:' + data)
   print('Connection closed')
 
 if __name__ == '__main__':
